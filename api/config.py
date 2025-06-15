aa79531bdcfac1dec6fff4af87bf6ad8a5a5a5b0
Owolabi Adebayo <qad4luv@gmail.com>
2025-06-15 10:32:47 -0700

    Initial RSI Scanner commit

---
+++ api/config.py
@@ -0,0 +1,5 @@
+import os
+
+# These values should be set as environment variables on Vercel
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "your-bot-token")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID", "your-chat-id")
